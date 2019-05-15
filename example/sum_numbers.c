cd1be4c7e3fac9283a702b355074424630a4113b
Luke Craig <lacraig3@gmail.com>
2019-05-15 12:34:13 -0400

    Update sum_numbers.c

--- example/sum_numbers.c
+++ example/sum_numbers.c
@@ -1,12 +1,4 @@
-int sum_numbers(int num_numbers, int* numbers, int (*printout_num)(const void *)){
-	int i, sum = 0;
-	for (i=0; i< num_numbers; i++){
-		sum +=numbers[i];
-		printout_num(numbers[i]);
-	}
-	return sum;
-}
-
+#include "panda/plugin.h"
 
 int on_asid_change(CPUState *env, int old_asid, int new_asid){
     printf("ASID CHANGED from %u to %u", old_asid, new_asid);
@@ -17,4 +9,15 @@ bool init_plugin(void *self){
   panda_cb pcb = {.asid_changed = on_asid_change};
   panda_register_callback(self, PANDA_CB_ASID_CHANGED, pcb};
   return true;
+}
+
+			  
+			  
+int sum_numbers(int num_numbers, int* numbers, int (*printout_num)(const void *)){
+	int i, sum = 0;
+	for (i=0; i< num_numbers; i++){
+		sum +=numbers[i];
+		printout_num(numbers[i]);
+	}
+	return sum;
 }
