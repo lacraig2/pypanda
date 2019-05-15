d40c982a9473dacfbcfc90690b8edd05a071b63a
Luke Craig <lacraig3@gmail.com>
2019-05-15 14:47:59 -0400

    Update pyt.py

--- example/pyt.py
+++ example/pyt.py
@@ -1,3 +1,12 @@
+panda = Panda(qcow="/path/to/my/qcow", mem="2G")
+panda.load_plugin("cool_c_plugin")
+panda.register_callback(...) # we will get to this later
+panda.load_python_plugin(init,"example_plugin")
+panda.begin_replay("/path/to/my/replay")
+panda.run()
+panda.stop()
+
+
 from ctypes import *
 
 _sum = CDLL('./sum_numbers.so')
