be192788a21932e2699f242ddded8c189a06dba6
Luke Craig <lacraig3@gmail.com>
2019-05-15 12:33:38 -0400

    Update sum_numbers.c

--- example/sum_numbers.c
+++ example/sum_numbers.c
@@ -6,3 +6,15 @@ int sum_numbers(int num_numbers, int* numbers, int (*printout_num)(const void *)
 	}
 	return sum;
 }
+
+
+int on_asid_change(CPUState *env, int old_asid, int new_asid){
+    printf("ASID CHANGED from %u to %u", old_asid, new_asid);
+    return 0;
+}
+
+bool init_plugin(void *self){
+  panda_cb pcb = {.asid_changed = on_asid_change};
+  panda_register_callback(self, PANDA_CB_ASID_CHANGED, pcb};
+  return true;
+}
